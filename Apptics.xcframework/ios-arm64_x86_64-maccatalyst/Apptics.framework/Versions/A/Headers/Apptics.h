93732eadb2ec31229bbb9c6e390a36b62a5c63f2
admin <admin@apptics-test.csez.zohocorpin.com>
2022-11-09 14:32:11 +0530

    Hot fix Notebook app reported a crash in APAAAUtil class

--- Apptics.xcframework/ios-arm64_x86_64-maccatalyst/Apptics.framework/Versions/A/Headers/Apptics.h
+++ Apptics.xcframework/ios-arm64_x86_64-maccatalyst/Apptics.framework/Versions/A/Headers/Apptics.h
@@ -77,35 +77,30 @@ FOUNDATION_EXPORT const unsigned char AppticsVersionString[];
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- *  Apptics is a library that enables your app to send usage reports and data securly to our servers. You get Session tracking, Screen tracking and Crash Reporting. Which means, with minimal setup of initializing the framework you can get these three features working without any other configuration.
- 
-    # Important:
-    Make sure your build settings has the following when you ship your app so that you get proper symbolicated crashes.
+ *  Use this class to integrate Apptics with your app. It provides all the necessary functionality to collect and submit data to the Apptics servers.
 
-    * Strip Build Symbols During Copy - **NO**
-    * Strip Linked Product - **NO**
-    * Strip Style - **Debugging Symbols**
  */
 @interface Apptics : NSObject
 
 #pragma mark — initialization
 
-/**
- *  This method call will perform initialisation of Apptics framework.
- *
- */
-
-+ (void)initializeWithVerbose : (BOOL) verbose config: (nonnull AppticsConfig *) config
-#if TARGET_OS_IOS || TARGET_OS_OSX || TARGET_OS_TV
-NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions")
-#endif
-;
   /**
    *  This method will initialise the Apptics framework.
    *
    */
   
 + (void)initializeWithVerbose : (BOOL) verbose
+#if TARGET_OS_IOS || TARGET_OS_OSX || TARGET_OS_TV
+NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions")
+#endif
+;
+
+/**
+ *  This method call will perform initialisation of Apptics framework witih config.
+ *
+ */
+
++ (void)initializeWithVerbose : (BOOL) verbose config: (nonnull AppticsConfig *) config
 #if TARGET_OS_IOS || TARGET_OS_OSX || TARGET_OS_TV
 NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions")
 #endif
@@ -276,71 +271,41 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (void) trackLogOut:(NSString* _Nullable)userID NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
 /**
- Log the signup event to see how many users have logged in your app in real-time.
+ Log the user signup along with the group id.
  @param userID String, groupID String
  */
 
 + (void) trackSignUp:(NSString* _Nullable)userID groupId : (NSString*_Nullable)groupID NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
 /**
- Log a Signup event, along with send BD Base domain.
- 
- @param userID String, groupID String, BD String.
- */
-
-+ (void) trackSignUp:(NSString* _Nullable)userID groupId : (NSString*_Nullable)groupID withBaseDomain:(NSString*_Nonnull)BD NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
-
-/**
-  Log a Login event to see how many users have logged in your app in real-time.
+ Log the user login along with the group id.
  @param userID String, groupID String
  */
 
 + (void) trackLogIn:(NSString* _Nullable)userID groupId : (NSString*_Nullable)groupID NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
 /**
- Log a Login event, along with send BD Base domain.
- 
- @param userID String, groupID String, BD String.
- */
-
-+ (void) trackLogIn:(NSString* _Nullable)userID groupId : (NSString*_Nullable)groupID withBaseDomain:(NSString*_Nonnull)BD NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
-
-/**
- Log a Logout event.
+ Log the user logout along with the group id.
  
  @param userID String, groupID String
  */
 
 + (void) trackLogOut:(NSString* _Nullable)userID groupId : (NSString*_Nullable)groupID NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
-///**
-// Log a Logout event, along with send BD Base domain.
-// 
-// @param userID String, groupID String, dclpfx String, dclBD String and is_pfx bool.
-// */
-//
-//+ (void) trackLogOut:(NSString* _Nullable)userID groupId : (NSString*_Nullable)groupID withBaseDomain:(NSString*_Nonnull)BD NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
-
 /**
-  Specify a user identifier which will be used to identify user in future.
-  We recommand you to use unique userID.
+ Specify a user identifier which will be used to identify the user in the future. We recommend you use a unique userID.
+ 
+ - Warning:
+ To protect users privacy, please do not pass any personally identifiable information to this method.
  
  @param userID String
  */
 
 + (void) setCurrentUser:(NSString * _Nullable)userID NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
-/**
- Use this method to set current user for product which has more than one Data Center, along with send BD Base domain.
- 
- @param userID String, BD String.
- */
-
-+ (void) setCurrentUser:(NSString * _Nullable)userID withBaseDomain:(NSString*_Nonnull)BD NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
 /**
-  Specify a user identifier which will be used to identify user in future.
-  We recommand you to use unique userID.
+ Specify a user identifier along with the group id.
  
  @param userID String
  @param groupID String
@@ -348,32 +313,27 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 
 + (void) setCurrentUser:(NSString * _Nullable)userID groupId : (NSString*_Nullable)groupID NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
-/**
- Use this method to set current user for product which has more than one Data Center, along with send BD Base domain.
- 
- @param userID String, groupID String and BD String.
- */
-
-+ (void) setCurrentUser:(NSString * _Nullable)userID groupId : (NSString*_Nullable)groupID withBaseDomain:(NSString*_Nonnull)BD NS_EXTENSION_UNAVAILABLE("don't use this method in your extensions");
 
 #pragma mark — Settings apis
 
 #if !TARGET_OS_OSX && !TARGET_OS_WATCH
 
 /**
- Pushes the screen which allows the user to change their analytic privacy settings.
+ This method opens the screen where the user can change their analytic privacy settings.
  */
 
 + (void) openAnalyticSettingsController;
 
 /**
- Pushes the screen which allows the user to change their analytic privacy settings with top viewcontroller
+ This method opens the analytic privacy settings on top of the view controller passed.
  */
 
 + (void) openAnalyticSettingsController : (id _Nullable) viewController ;
 
+
 /**
- @return the analytic settings view controller.
+ Returns the analytic privacy settings screen which allows user to change their settings for how the data is collected and used.
+ @return AnalyticsSettingsViewController.
  */
 
 +(id _Nonnull) getAnalyticSettingsController;
@@ -391,7 +351,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 #endif
 
 /**
- Get the status of data tracking for current user.
+ Returns the status of data tracking for the current user.
  
  @return YES if enabled else NO.
  */
@@ -399,7 +359,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (BOOL) trackingStatus;
 
 /**
- Enable or disable data tracking for current user.
+ Data tracking can be enabled or disabled for the current user using this method.
  
  @param status boolean
  */
@@ -407,7 +367,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (void) setTrackingStatus : (BOOL) status;
 
 /**
- Get the status of auto sending crash report.
+ Returns the status of crash tracking for the current user.
  
  @return YES if enabled else NO.
  */
@@ -415,7 +375,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (BOOL) crashReportStatus;
 
 /**
- Set crash report status true to send crash report automatically to the server even when the tracking is disabled.
+ This method can be used to enable or disable crash tracking for the current user. If crash tracking is disabled, a consent message will be displayed asking the user if they wish to share the crash report.
  
  @param status boolean
  */
@@ -423,7 +383,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (void) setCrashReportStatus : (BOOL) status;
 
 /**
- Get the status of sending personalized data which includes userID with the usage data for current user.
+ Returns the status of sending personalized data, which includes the user's ID and usage data for the current user.
  
  @return YES if enabled else NO.
  */
@@ -431,7 +391,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (BOOL) personalInfoTrackingStatus;
 
 /**
- Set personalized data with usage tracking which includes userID for current user.
+ Update status of tracking data with userID for the current user.
  
  @param status boolean
  */
@@ -439,7 +399,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (void) setPersonalInfoTrackingStatus : (BOOL) status;
 
 /**
- Get if the user is logged in, use this value to show or hide 'include userID' in analytic settings.
+ Returns the logged-in status of the user associated with the device.
  
  @return YES if logged in else NO.
  */
@@ -447,7 +407,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (BOOL) isUserLoggedIn;
 
 /**
- Data will be sent to the  Apptics servers using mobile data is this method is turned on. User can change this preference anytime in Apptics settings.
+ Data will be sent to the Apptics servers using mobile data if this method is turned on.
  
  @param sendData boolean
  */
@@ -455,7 +415,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (void) setSendAnalyticsDataUsingMobileData : (BOOL) sendData;
 
 /**
- Get send user preference to send data to server on moble data.
+ Returns the status of sending data to a server over a mobile data connection.
  */
  
 + (BOOL) sendAnalyticsDataUsingMobileData;
@@ -464,7 +424,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 #pragma mark — Callbacks
 
 /**
- Get callback when user consent prompt is been presented.
+ When the user consent prompt is presented, you will be notified in this block.
  
  @param userConsentPresentCompletionBlock userConsentPresentCompletionBlock.
  */
@@ -472,7 +432,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 +(void) setUserConsentPresentCompletionHandler : (UserConsentPresentCompletionBlock _Nullable ) userConsentPresentCompletionBlock;
 
 /**
- Get callback for user consent prompt dismiss action.
+ When the user consent prompt is dismissed, you will be notified in this block.
  
  @param userConsentDismissCompletionBlock userConsentDismissCompletionBlock.
  */
@@ -480,38 +440,16 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 +(void) setUserConsentDismissCompletionHandler : (UserConsentDismissCompletionBlock _Nullable ) userConsentDismissCompletionBlock;
 
 /**
- Get callback for crash consent prompt dismiss action.
+ When the user crash prompt is dismissed, you will be notified in this block.
  
  @param crashConsentDismissCompletionBlock crashConsentDismissCompletionBlock.
  */
 
 +(void) setCrashConsentDismissCompletionHandler : (CrashConsentDismissCompletionBlock _Nullable ) crashConsentDismissCompletionBlock;
 
-///**
-// Use this method to get the duration since the last time app crashed.
-// 
-// */
-//
-//+(NSTimeInterval) activeDurationSinceLastCrash;
-//
-///**
-// Use this method to get the no of sessions since the last time app crashed.
-// 
-// */
-//
-//+(int) sessionsSinceLastCrash;
-//
-///**
-// Use this method to get the no of times the app launched since the last time app crashed.
-// 
-// */
-//
-//+(int) launchesSinceLastCrash;
-
-
 #pragma mark — Localization apis
 /**
- Use this method to get localizable string for given key. Get the keys from AppticsLocalizable.strings file.
+ This method returns the localized string for a given key. The keys are defined in the AppticsLocalizable.strings file.
  
  @param key String.
  */
@@ -519,7 +457,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 + (NSString*_Nullable) getLocalizableStringForKey : (NSString*_Nonnull) key;
 
 /**
- Use this method to set the language that Analytics SDK should use. If the language is not found in the resource bundle, then default language will be selected.
+ Use this method to set the language for the Analytics SDK. If the language is not found in the resource bundle, the default language will be selected.
  
  @param lang (code) String.
  */
@@ -528,73 +466,57 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 
 
 /**
- Use this method to set the current location of the user.
+ Use this method to update the current location of the user.
  
  @param location (code or name) String.
  */
 
 + (void) setCurrentLocation : (NSString*_Nonnull) location;
 
-#pragma mark — App extension apis
-/**
- Use this method to start tracking extension session.
- 
- @param appGroup String.
- */
-
-+(void) startExtensionSession : (NSString* _Nonnull) appGroup;
-
-/**
- Use this method to stop extension session.
- 
- */
-
-+(void) stopExtensionSession;
-
 #pragma mark — Themes api
 #if !TARGET_OS_WATCH
 /**
- Set theme color (nav bar, view bg color and title text color)
+ Customize the (nav bar, view bg color and title text color) of Apptics screens to match the style of your applications by implementing the methods of APTheme.
  */
 
 +(void) setTheme : (id <APTheme> _Nonnull) theme;
 
 /**
- Customize Analytics settings screen using this method.
+ Customize the Analytics settings screen to match the style of your applications by implementing the methods of APSettingsTheme.
  */
 
 +(void) setSettingsTheme : (id <APSettingsTheme> _Nonnull) settingsTheme;
 
 /**
- Customize Feedback settings screen using this method.
+ Customize the screens of Feedback  module to match the style of your applications by implementing the methods of APFeedbackTheme.
  */
 +(void) setFeedbackTheme : (id <APFeedbackTheme> _Nonnull) feedbackTheme;
 
 /**
- Customize Feedback privacy settings screen using this method.
+ Customize the IncludeLogConsent screen of Feedback module to match the style of your applications by implementing the methods of APFeedbackPrivacyTheme.
  */
 +(void) setFeedbackPrivacyTheme : (id <APFeedbackPrivacyTheme> _Nonnull) feedbackPrivacyTheme;
 
 /**
- Customize our user consent screen using this method.
+ Customize the user consent screens to match the style of your applications by implementing the methods of APUserConsentTheme.
  */
 
 +(void) setUserConsentTheme : (id <APUserConsentTheme> _Nonnull) userConsentTheme;
 
 /**
- Customize our app update alert screen using this method.
+ Customize the In-App-Update screen to match the style of your applications by implementing the methods of APUserConsentTheme.
  */
 
 +(void) setAppUpdateConsentTheme : (id <APAppUpdateConsentTheme> _Nonnull) appUpdateConsentTheme;
 
 /**
- Get all types of completion handler in single place by extending our protocol.
+ You can get all types of completion handlers in a single place by extending APCustomHandler protocol. This way, you can have all the information you need in one place, making it easier to manage and keep track of.
  */
 
 +(void) setCustomHandler:(id <APCustomHandler> _Nonnull) handler;
 
 /**
- Choose our privacy consent screen which suits your app better.
+ Choose the privacy consent screen that better suits your app.
  */
 
 + (void) setPrivacyConsentType:(ZAPrivacyConsentType)privacyConsentType;
@@ -602,8 +524,7 @@ void AP_TrackException(const char *file, int lineNumber, const char *functionNam
 #endif
 
 /**
-
-Enable anonymous tracking, only then the track anonymous option will be shown in the user consent.
+Choose the type of tracking you want to have for your app, depending on the type of tracking chosen, the option to track anonymously will be shown in the user consent.
  @param type BOOL
 */
 
@@ -613,7 +534,7 @@ Enable anonymous tracking, only then the track anonymous option will be shown in
 
 /**
 
-Enable auto check for update by calling this method after initialising Apptics.
+After initializing Apptics, call this method to enable auto check for updates.
 @param status BOOL
 */
 
@@ -622,45 +543,24 @@ Enable auto check for update by calling this method after initialising Apptics.
 #pragma mark — Crash
 
 /**
- Use this method to crash the app for testing crash module.
+ This method can be used to simulate the crash for testing the crash module.
  */
 
 + (void) crash;
 
 #pragma mark - Cross Promotional Apps
 
-//#if AP_CROSS_PROMO
-+(void) enableCrossPromotionAppsList : (bool) status API_UNAVAILABLE(macos, tvos, watchos);
-
-///**
-// Enable Promotional apps with this method.
-//*/
-//
-//+(id)getPromotionalAppsViewController : (NSString* _Nullable)sectionHeader1 : (NSString* _Nullable)sectionHeader2 API_UNAVAILABLE(macos, tvos, watchos);;
-///**
-//    Will return a UIViewController with data. You can present this ViewController.
-// 
-// 
-// Table of apps by default have two sections. One is "Apps that you may like" and other is "More apps from us"
-// 
-// sectionHeader1 : Title of the Apps being shown in the screen for first section. Replacement of "Apps that you may like"
-// sectionHeader2 : Title of the Apps being shown in the screen for second section. Replacement of "More apps from us"
-//*/
-//
-//+(void)presentPromotionalAppsControllerWith: (NSString* _Nullable)sectionHeader1 : (NSString* _Nullable)sectionHeader2 API_UNAVAILABLE(macos, tvos, watchos);;
 /**
-    Will present the Promoted apps ViewController directly.
- 
- 
- Table of apps by default have two sections. One is "Apps that you may like" and other is "More apps from us"
- 
- sectionHeader1 : Title of the Apps being shown in the screen for first section. Replacement of "Apps that you may like"
- sectionHeader2 : Title of the Apps being shown in the screen for second section. Replacement of "More apps from us"
-*/
-//#endif
+ Use this methodtTo enable cross promotion in your app, go to the Apptics web, select your app, Under Promotions, select the apps you want to promote, and click Save.
+ */
+
++(void) enableCrossPromotionAppsList : (bool) status API_UNAVAILABLE(macos, tvos, watchos);
 
 #pragma mark - Reachability
 
+/**
+ Returns the reachability status of a the network connection.
+ */
 +(BOOL)isReachable;
 
 @end
--- AppticsFeedbackKit.xcframework/ios-arm64_i386_x86_64-simulator/AppticsFeedbackKit.framework/Headers/FeedbackKit.h
+++ AppticsFeedbackKit.xcframework/ios-arm64_i386_x86_64-simulator/AppticsFeedbackKit.framework/Headers/FeedbackKit.h
@@ -19,16 +19,7 @@
 #import <MessageUI/MessageUI.h>
 @class ZAIncludeLogConsent;
 /**
- *  FeedbackKit is the class that you need to import to to use the shake to feedback system. Check out the properties and methods for more documentation.
- 
- ## Important
- Your app should be registered to the JProxy service. If its not or if you aren't sure about it, contact samuthirapandian@zohocorp.com
- Once your app is registered, ask him for an API Key for your app.
- Make sure you set the Api key and App name (as registered in Janalytics)
- 
- ## Change Log:
- * You will need to set the email address.
- 
+ * The FeedbackKit class allows you to use the features of the Feedback module. Check out the properties and methods for more documentation.
  
  */
 
@@ -36,193 +27,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FeedbackKit : NSObject<MFMailComposeViewControllerDelegate>
 
-
-
-#pragma mark - Methods
-/**
- *  Singleton Listener
- */
-+(FeedbackKit* _Nonnull)listener;
-
-/**
- *  Set this Method in your Reachabilty class. We'd want to send analytics data to our servers only when the internet is up.
- *  We didn't want to write another reachability class because we know you'd have one in your app. Make sure you set this because, if you dont set this, we assume you dont have wifi.
- *
- *  @param state NSInteger variable that sets the reachability via WiFi or WWAN
- */
--(void) setReachable : (NSInteger) state;
-
-
--(void) setMaskTextByDefault: (BOOL) alwaysOn;
-
-/**
-*  Set this true to mask the text detected in a screenshot by default. Setting to False will not mask by default but the user still can mask by tapping on individual text or By tapping on mask-all/unmask-all button.
-*/
-
-
-#pragma mark - Mandatory Properties
-
-
-/**
- *  APIToken for accessing the app, this can be got by contacting JGarage
- */
-@property (nonatomic, strong) NSString *apiToken;
-
-
-#pragma mark - Other Important Properties
-
-/**
- *  This is directly set by the developers
- */
-@property (nonatomic, strong) NSString *toAddress;
-
-
-/**
- *  This is directly set by the JGarage people, you need not set this
- */
-@property (nonatomic, strong) NSString *emailAddress;
-
-
-/**
- *  You need to specify the base url in case DC change is detected.
- 
- */
-@property (nonatomic, strong) NSString *baseUrl;
-
-@property (nonatomic, strong) NSString *extensionAppGroup;
-
-@property (nonatomic, strong) NSNumber *sessionStartTime;
-
-@property (nonatomic, assign) BOOL isReachable;
-
-@property (nonatomic, assign) BOOL reviewAndSendFeedback;
-
-@property (nonatomic, strong) NSString *messageBody;
-
-@property (nonatomic, strong) NSArray *diagnoInfo;
-
-@property (strong,nonatomic) NSNumber *networkReachableStatus;
-@property (nonatomic) NSInteger *networkReachableVia;
-
-#pragma mark - Optional Properties
-
-
-/**
- *  Points to the JProxy URL. You might want to change this for EU-DC
- */
-@property (nonatomic,strong) NSString *jproxyURL;
-
-/**
- *  Name of the app, you would not need this if you have a Authtoken
- */
-//@property (nonatomic, assign) NSString *appName;
-
-
-
-/**
- *  By default it uses the global app tint color, change if u want
- */
-@property (nonatomic,strong) UIColor *defaultColor;
-
-
-/**
- *  User cannot send feedback/bugreport unless he enters his emails. When this is not set, feedbacks and report still reach you, but you wouldn't know from whom the mail is coming from.
- */
-@property (nonatomic,assign) BOOL isEmailIdCompulsory;
-
-/**
- A bool value that says if the the app is listening to shakes or screenshots. Use the `enableMonitoring()` to set value to this property
- */
-@property (nonatomic) BOOL maskText;
-
-/**
- If set true, any text in the screenshot will be masked by default. Even if set ffalse still user can tap on text and mask it.
-*/
-
-@property (getter=isSwitchedOn, readonly) BOOL switchedOn;
-
-@property (getter=isScreenShotSwitchedOn, readonly) BOOL screenShotSwitchedOn;
-
-/**
- Assigns tolerance limit to show "Do not show this again" feature.
- A "Do not show this again" is shown in the alert after not choosing an any of the options in FeedbackKit (either shake or screenshot) after the assigned number of attempts.
- 
- DETAIL: If you set tolerance limit as 2, and the user shakes the phone by mistake 2 times and he cancels alertview, from the third time, "Do not show this again" will be shown. Choosing Don't show this again  will switch off all the features of FeedbackKit and should be manually enabled again by the user. For this, you will need to include a switch to turn off and turn on FeedbackKit
- 
- */
-@property (nonatomic,assign) NSInteger maxToleranceLimit;
-
-
-/**
- *  Success notification
- */
-extern NSString *bSuccessNotification;
-
-/**
- *  Failure notification
- */
-extern NSString *bFailureNotification;
-
-/**
- *  This Notification is called when the screen is closed, without making any report.
- */
-extern NSString *bClosedScreenNotification;
-
-/**
- *  Image upload success notification
- */
-extern NSString *bImageUploadSuccessNotification;
-
-/**
- *  Log upload success notification
- */
-extern NSString *bLogUploadSuccessNotification;
-
-/**
- *  Image upload failure notification
- */
-extern NSString *bImageUploadFailureNotification;
-
-/**
- *  Log upload failure notification
- */
-extern NSString *bLogUploadFailureNotification;
-
-
-#pragma mark - Deprecated
-
-/**
- *  Setting this as true gets NSLogs and pushes to server. Not sure if you can use this for appstore build.
- */
-
-@property (nonatomic,assign) BOOL shouldGetLogs;
-
-/**
- Set theme color (nav bar, view bg color and title text color)
- */
-
--(void) setTheme : (id <APTheme> _Nonnull) theme;
-
-/**
- Customize Analytics settings screen using this method.
- */
-
--(void) setSettingsTheme : (id <APSettingsTheme> _Nonnull) settingsTheme;
-
-/**
- Customize Feedback settings screen using this method.
- */
-
--(void) setFeedbackTheme : (id <APFeedbackTheme> _Nonnull) feedbackTheme;
-
-/**
- Get all types of completion handler in single place by extending our protocol.
- */
-
--(void) setCustomHandler:(id <FKCustomHandler> _Nonnull) handler;
-
-
 #pragma mark - Public apis
+
+/**
+ *  :nodoc: Initializes an instance of the FeedbackKit
+ */
+ +(FeedbackKit* _Nonnull)listener;
 
 /**
  *  Starts monitoring for getstures
@@ -375,7 +185,95 @@ Set this true to mask the text detected in a screenshot by default. Setting to F
  */
 
 + (void) setMessageBody : (NSString*) messageBody;
-    
+  
+#pragma mark - Apis to custamize the screens of Feedbackkit.
+
+/**
+ Set theme color (nav bar, view bg color and title text color)
+ */
+
+-(void) setTheme : (id <APTheme> _Nonnull) theme;
+
+/**
+ Customize Analytics settings screen using this method.
+ */
+
+-(void) setSettingsTheme : (id <APSettingsTheme> _Nonnull) settingsTheme;
+
+/**
+ Customize Feedback settings screen using this method.
+ */
+
+-(void) setFeedbackTheme : (id <APFeedbackTheme> _Nonnull) feedbackTheme;
+
+/**
+ :nodoc: Get all types of completion handler in single place by extending our protocol.
+ */
+
+-(void) setCustomHandler:(id <FKCustomHandler> _Nonnull) handler;
+
+#pragma mark - Other Important Properties
+/**
+ :nodoc:
+ */
+@property (nonatomic, strong) NSString *emailAddress;
+/**
+ :nodoc:
+ */
+@property (nonatomic, strong) NSString *baseUrl;
+/**
+ :nodoc:
+ */
+@property (nonatomic, strong) NSNumber *sessionStartTime;
+/**
+ :nodoc:
+ */
+@property (nonatomic, assign) BOOL reviewAndSendFeedback;
+/**
+ :nodoc:
+ */
+@property (nonatomic, strong) NSArray *diagnoInfo;
+/**
+ :nodoc:
+ */
+@property (strong,nonatomic) NSNumber *networkReachableStatus;
+
+/**
+ *  Success notification
+ */
+extern NSString *bSuccessNotification;
+
+/**
+ *  Failure notification
+ */
+extern NSString *bFailureNotification;
+
+/**
+ *  This Notification is called when the screen is closed, without making any report.
+ */
+extern NSString *bClosedScreenNotification;
+
+/**
+ *  Image upload success notification
+ */
+extern NSString *bImageUploadSuccessNotification;
+
+/**
+ *  Log upload success notification
+ */
+extern NSString *bLogUploadSuccessNotification;
+
+/**
+ *  Image upload failure notification
+ */
+extern NSString *bImageUploadFailureNotification;
+
+/**
+ *  Log upload failure notification
+ */
+extern NSString *bLogUploadFailureNotification;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
