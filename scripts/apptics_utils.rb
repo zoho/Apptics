d2c31ff2d0f7e1d44b7fbdfeeac316fdf1ddaecf
admin <admin@apptics-test.csez.zohocorpin.com>
2022-12-16 12:46:49 +0530

    Pushing 1.1.1d changes

--- scripts/apptics_utils.rb
+++ scripts/apptics_utils.rb
@@ -2,7 +2,7 @@ begin
 require "net/http"
 require "json"
 rescue LoadError => error
-  puts "echo #{error.message}, check if commandline tools are installed"
+  puts "Error: #{error.message}, check if commandline tools are installed"
   exit
 end
 
@@ -12,10 +12,15 @@ class AppticsBot
     
     data_hash = JSON.parse(File.read(filePath))
     
+    if data_hash["result"] == "failure"
+        puts "Error: #{data_hash['error_message']}"
+        return false
+    end
+  
     data_hash = data_hash["data"]
     
     if data_hash == nil
-      puts "echo 'Invalid entries found in the response'"
+      puts "Error: 'Invalid entries found in the response : #{data_hash}'"
       return false
     end
        
@@ -47,32 +52,40 @@ class AppticsUploader
     end
 
     def self.upload()
-        url = URI.parse(@urlstring)
-        File.open(@filepath) do |zip|
-          req = Net::HTTP::Post.new(url)
-          form_data = [['dsymfile', zip]]
-          req.set_form form_data, 'multipart/form-data'
-          req['zak']=@token
-          res = Net::HTTP.start(url.host, url.port, use_ssl: true, open_timeout:60, read_timeout: 300) do |http|
-          http.request(req)
-          end
-          
-        # Status
-        puts res.code       # => '200'
-        puts res.class.name # => 'HTTPOK'
+        
+        begin
+          # potentially exception raising code
+          url = URI.parse(@urlstring)
+          File.open(@filepath) do |zip|
+            req = Net::HTTP::Post.new(url)
+            form_data = [['dsymfile', zip]]
+            req.set_form form_data, 'multipart/form-data'
+            req['zak']=@token
+            res = Net::HTTP.start(url.host, url.port, use_ssl: true, open_timeout:60, read_timeout: 300) do |http|
+            http.request(req)
+            end
+            
+          # Status
+  #        puts res.code       # => '200'
+  #        puts res.class.name # => 'HTTPOK'
 
-        # Body
-        if valid_json?(res.body)
-        data_hash = JSON.parse(res.body)
-        puts "dSYM upload status : #{data_hash['result']}"
-        if data_hash["result"] == "success"
-              puts "data : #{data_hash['data']}"
-              else
-              puts "error_message : #{data_hash['error_message']}"
+          # Body
+          
+          if valid_json?(res.body)
+          data_hash = JSON.parse(res.body)
+          if data_hash["result"] == "success"
+                puts "Success: #{data_hash['data']}"
+                else
+                puts "Error: #{data_hash['error_message']}"
+          end
+          else
+              puts "Error: #{res.body}"
+          end
+          end
+        rescue StandardError => e
+          # run code if StandardError or anything that inherits from StandardError is raised (most errors)
+          puts "Error: #{e}"
         end
-        else
-          puts res.body
-        end
-        end
+                
     end
 end
