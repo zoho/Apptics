08a3bce4077facb44af6d2d03abf6b7b13c667e6
apptics-test <appticts-0639-t@appticts-0639-t.csez.zohocorpin.com>
2026-05-05 16:41:51 +0530

    Published beta 3.3.14001

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.6
  import PackageDescription
- let version = "3.3.13"
+ let version = "3.3.14001"
  let package = Package(
      name: "Apptics",
      defaultLocalization: "en",
