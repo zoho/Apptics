910ec210458f42e0accc3baf23db7b311442e037
apptics-test <ssaravanan@zohocorp.com>
2026-07-03 17:15:54 +0530

    Published 3.3.16

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.6
  import PackageDescription
- let version = "3.3.15"
+ let version = "3.3.16"
  let package = Package(
      name: "Apptics",
      defaultLocalization: "en",
